612f8e0a4dede517b3e777032040bcd304a20d6c
ciniciato <diegociniciato1@hotmail.com>
2016-08-23 17:07:55 -0300

    ip

--- assets/js/control.js
+++ assets/js/control.js
@@ -5,42 +5,11 @@ var control = {
 
 //<input type="file" accept="image/*;capture=camera"> tirar foto
 
-function start() {
-  if (window.stream) {
-    window.stream.getTracks().forEach(function(track) {
-      track.stop();
-    });
-  }
-  var videoSource = videoSelect.value;
-  var constraints = {
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-  };
-  navigator.mediaDevices.getUserMedia(constraints).
-      then(gotStream).then(gotDevices).catch(handleError);
-}
-
-control.step = function(){
-    compatibility.requestAnimationFrame(control.step);
-	if (canvas.video.readyState === canvas.video.HAVE_ENOUGH_DATA) {
-		var
-		size=canvas.w*canvas.h,
-		mapmag = new Int32Array(size), 
-		mapdir = {x: new Int32Array(size),
-					y: new Int32Array(size)};
-
-		canvas.ctx.drawImage(canvas.video, 0, 0, canvas.w, canvas.h);
-		var imgdata = canvas.ctx.getImageData(0, 0, canvas.w, canvas.h);
-		//input = imgdata.getGrayChannel();
-		//fastSobel(input, mapmag, mapdir, canvas.w,canvas.h);
-		//imgdata.setGrayChannel(mapmag);
-		canvas.ctx.putImageData(imgdata, 0, 0);
-	}
-}
-
 control.init = function(){
 	view.init();
 	document.getElementById('openFile').addEventListener('change', control.loadPhoto, false);
-	document.getElementById('btn_camera').addEventListener('click', view.changeCamera, false);
+	document.getElementById('btn_changecamera').addEventListener('click', view.changeCamera, false);
+	document.getElementById('btn_realtime').addEventListener('click', view.setRealTime, false);
 }
 		
 control.takePhoto = function(){
